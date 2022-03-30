b3c1b8ce4b7fa0a672636187d4ad2e64f67622e6
Marcelo Hernández <marcelo.hernandezu99@gmail.com>
2022-03-30 15:53:44 -0600

    probando url

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -76,7 +76,7 @@ class LoginController {
 
                     //  Enviar el email
                     $email = new Email($usuario->email, $usuario->nombre, $usuario->token);
-                    $root = explode("?", $_SERVER["REQUEST_URI"])[0];
+                    $root = $_SERVER["REQUEST_SCHEME"] . "://" . $_SERVER["HTTP_HOST"];
                     $email->setRoot($root);
                     $email->enviarInstrucciones();
 
@@ -162,7 +162,7 @@ class LoginController {
 
                     // Enviar el Email
                     $email = new Email($usuario->nombre, $usuario->email, $usuario->token);
-                    $root = explode("?", $_SERVER["REQUEST_URI"])[0];
+                    $root = $root = $_SERVER["REQUEST_SCHEME"] . "://" . $_SERVER["HTTP_HOST"];
                     $email->setRoot($root);
                     $email->enviarConfirmacion();
 
